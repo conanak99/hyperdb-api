ee676a56899cb22cec134aa44be003adb3a47ccf
conanak99 <huyhoang8a5@gmail.com>
2023-06-09 17:41:24 +0300

    Start adding lol

--- README.md
+++ README.md
@@ -10,8 +10,10 @@ pip install -r requirements.txt
 
 ## Usage
 
-Dev
+Dev. Visit <http://127.0.0.1:8000/docs> or <http://127.0.0.1:8000/redoc> for document
 
 ```bash
 uvicorn main:app --reload
 ```
+
+## Docker
--- main.py
+++ main.py
@@ -1,15 +1,81 @@
-from typing import Union
-
-from fastapi import FastAPI
+import uuid
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Dict, List
+from hyperdb import HyperDB
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Define the data models
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+class DB_Input(BaseModel):
+    name: str
+    key: str
+    metadata: str
+
+    class Config:
+        frozen = True
+
+
+class DB(BaseModel):
+    id: str
+    name: str
+    key: str
+    metadata: str
+
+
+# Define the database
+databases = {
+
+}
+
+databases_meta = {
+
+}
+
+# Create endpoints for CRUD operations
+
+
+@app.post("/db")
+async def create_db(db: DB_Input) -> DB:
+    # generate random ID lol
+    db_id = str(uuid.uuid4())
+    new_db: DB = {'id': db_id, 'name': db.name,
+                  'metadata': db.metadata, 'key': db.key}
+    databases[db_id] = {'id': db_id,
+                        'metadata': db.metadata, db: HyperDB(None, db.key)}
+    databases_meta[db_id] = new_db
+    return new_db
+
+
+@app.get("/db")
+async def read_users() -> List[DB]:
+    print(list(databases_meta.values()))
+    return list(databases_meta.values())
+
+
+@app.get("/db/{user_id}", response_model=DB)
+async def read_user(user_id: int):
+    for user in database["users"]:
+        if user.id == user_id:
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
+
+
+@app.put("/db/{user_id}", response_model=DB)
+async def update_user(user_id: int, user: Dict):
+    for i, user_data in enumerate(database["users"]):
+        if user_data["id"] == user_id:
+            database["users"][i] = user
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
+
+
+@app.delete("/db/{user_id}")
+async def delete_user(user_id: int):
+    for i, user_data in enumerate(database["users"]):
+        if user_data["id"] == user_id:
+            database["users"].pop(i)
+            return {"message": "User deleted successfully"}
+    raise HTTPException(status_code=404, detail="User not found")
