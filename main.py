fa2c9f57703b3f116cb5839eb796b47fc76158eb
conanak99 <huyhoang8a5@gmail.com>
2023-06-11 00:39:14 +0300

    update scaffold and create data folder

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import uuid
+import os
+import pathlib
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Dict, List
+from typing import Dict, List, Any
 from hyperdb import HyperDB
+from datetime import datetime
 
 app = FastAPI()
 
@@ -12,70 +15,76 @@ app = FastAPI()
 class DB_Input(BaseModel):
     name: str
     key: str
-    metadata: str
-
-    # class Config:
-    #     frozen = True
+    metadata: Any
 
 
-class DB(BaseModel):
+class DB_Meta(BaseModel):
     id: str
     name: str
     key: str
-    metadata: str
+    metadata: Any
+    created_at: datetime
+    last_sync: datetime
+    updated_at: datetime
 
 
 # Define the database
-databases = {
+databases: dict[str, HyperDB] = {
 
 }
 
-databases_meta = {
+databases_meta: dict[str, DB_Meta] = {
 
 }
+
+# Create data folder
+pathlib.Path('data').mkdir(parents=True, exist_ok=True)
 
 # Create endpoints for CRUD operations
 
 
-@app.post("/db")
-async def create_db(db: DB_Input) -> DB:
-    # generate random ID lol
-    db_id = str(uuid.uuid4())
-    new_db: DB = {'id': db_id, 'name': db.name,
-                  'metadata': db.metadata, 'key': db.key}
-    databases[db_id] = {'id': db_id,
-                        'metadata': db.metadata, db: HyperDB(None, db.key)}
-    databases_meta[db_id] = new_db
-    return new_db
-
-
 @app.get("/db")
-async def read_users() -> List[DB]:
-    print(list(databases_meta.values()))
+async def get_all_dbs() -> List[DB_Meta]:
     return list(databases_meta.values())
 
 
-@app.get("/db/{user_id}", response_model=DB)
-async def read_user(user_id: int):
-    for user in database["users"]:
-        if user.id == user_id:
-            return user
-    raise HTTPException(status_code=404, detail="User not found")
+@app.post("/db")
+async def create_db(db: DB_Input) -> DB_Meta:
+    # generate random ID lol
+    db_id = str(uuid.uuid4())
+    now = datetime.now()
+    new_db_meta: DB_Meta = DB_Meta(id=db_id, name=db.name, key=db.key,
+                                   metadata=db.metadata, created_at=now, updated_at=now, last_sync=now)
+    databases_meta[db_id] = new_db_meta
+
+    new_db = HyperDB(None, db.key)
+    new_db.save("data/" + db_id + ".pickle.gz")
+    databases[db_id] = new_db
+
+    return new_db_meta
 
 
-@app.put("/db/{user_id}", response_model=DB)
-async def update_user(user_id: int, user: Dict):
-    for i, user_data in enumerate(database["users"]):
-        if user_data["id"] == user_id:
-            database["users"][i] = user
-            return user
-    raise HTTPException(status_code=404, detail="User not found")
+# @app.get("/db/{user_id}", response_model=DB_Meta)
+# async def read_user(user_id: int):
+#     for user in database["users"]:
+#         if user.id == user_id:
+#             return user
+#     raise HTTPException(status_code=404, detail="User not found")
 
 
-@app.delete("/db/{user_id}")
-async def delete_user(user_id: int):
-    for i, user_data in enumerate(database["users"]):
-        if user_data["id"] == user_id:
-            database["users"].pop(i)
-            return {"message": "User deleted successfully"}
-    raise HTTPException(status_code=404, detail="User not found")
+# @app.put("/db/{user_id}", response_model=DB_Meta)
+# async def update_user(user_id: int, user: Dict):
+#     for i, user_data in enumerate(database["users"]):
+#         if user_data["id"] == user_id:
+#             database["users"][i] = user
+#             return user
+#     raise HTTPException(status_code=404, detail="User not found")
+
+
+# @app.delete("/db/{user_id}")
+# async def delete_user(user_id: int):
+#     for i, user_data in enumerate(database["users"]):
+#         if user_data["id"] == user_id:
+#             database["users"].pop(i)
+#             return {"message": "User deleted successfully"}
+#     raise HTTPException(status_code=404, detail="User not found")
